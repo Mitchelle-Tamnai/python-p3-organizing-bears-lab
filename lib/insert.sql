b111a6c43ac5c9bb2e4424291d53290fc13bd3a8
MitchelleTamnai <cheptoektamnai@gmail.com>
2024-09-17 21:37:51 +0300

    Commit changes

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, temperament, alive)
+VALUES ('Mr. Chocolate', 7, 'M', 'Light Brown', 'Friendly', TRUE),
+('Rowdy', 10, 'M', 'Black', 'Aggressive', TRUE),
+('Tabitha', 6, 'F', 'White', 'Aggressive', TRUE),
+('Sergeant Brown', 12, 'M', 'White', 'Calm', FALSE),
+('Melissa', 6, 'F', 'Dark Brown', 'Playful', TRUE),
+('Grinch', 9, 'M', 'Deep Red', 'Grinchy', TRUE),
+('Wendy', 7, 'F', 'Black', 'Gentle', TRUE),
+(NULL, 2, 'F', 'White', 'Timid', TRUE)
